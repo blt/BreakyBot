17fb764092e024146ac41b348a91c4ab6f2ccd30
Brian L. Troutwine <brian@troutwine.us>
2011-05-02 12:40:39 -0400

    BreakyBot now tracks Akka 1.1-SNAPSHOT.

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -2,7 +2,7 @@ import sbt._
 
 class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
   val akkaRepo = "Akka Repo" at "http://akka.io/repository"
-  val akkaPlugin = "se.scalablesolutions.akka" % "akka-sbt-plugin" % "1.1-M1"
+  val akkaPlugin = "se.scalablesolutions.akka" % "akka-sbt-plugin" % "1.1-SNAPSHOT"
 
   val bumRepo = "Bum Networks Release Repository" at "http://repo.bumnetworks.com/releases"
   val sbtAkkaBivy = "net.evilmonkeylabs" % "sbt-akka-bivy" % "0.2.0"
