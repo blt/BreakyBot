00f2207991194cc917bb0416d9b2af923723802a
Brian L. Troutwine <brian@troutwine.us>
2011-05-08 03:10:53 -0400

    Adapted akka's TestKit into TestParts; updated BreakyBotSpec to use it.

--- src/test/scala/BreakyBotSpec.scala
+++ src/test/scala/BreakyBotSpec.scala
@@ -2,50 +2,55 @@ package breakybot.spec
 
 import org.scalatest.{Spec,BeforeAndAfterAll,BeforeAndAfterEach}
 import akka.actor.Actor.actorOf
-import akka.testkit.TestKit
+import akka.testkit.TestParts
 import akka.util.duration._
 import akka.actor.ActorRef
 
 import breakybot.entity
 import breakybot.message.permission
 
-class BreakyBotSpec extends Spec with BeforeAndAfterAll with BeforeAndAfterEach with TestKit {
+class BreakyBotSpec extends Spec with BeforeAndAfterAll with BeforeAndAfterEach {
   private var machine:ActorRef = _
+  private val nanny:TestParts = new TestParts()
+  private val diary:TestParts = new TestParts()
 
   override def beforeEach() = {
-    machine = actorOf(new entity.BreakyBot(testActor, testActor)).start
+    machine = actorOf(new entity.BreakyBot(nanny.testActor, diary.testActor)).start
   }
   override def afterEach() = {
     machine.stop
   }
   override def afterAll() = {
-    stopTestActor
+    nanny.stopTestActor
+    diary.stopTestActor
   }
 
   describe("BreakyBot") {
     it("should request permission to dance with a new partner") {
       machine ! "Edgar Allen Poe"
-      expectMsg(50 millis, permission.Request)
-      expectNoMsg(10 millis)
+      nanny.expectMsg(50 millis, permission.Request)
+      nanny.expectNoMsg(10 millis)
     }
     it("should brag to its diary when it can dance with a new partner") {
       machine ! "Oscar Wilde"
-      expectMsg(50 millis) {
-        case permission.Request => testActor.reply( permission.Granted )
+      nanny.expectMsg(50 millis) {
+        // case permission.Request => nanny.testActor.reply( permission.Granted )
+        case permission.Request => machine ! permission.Granted
         case _ => fail
       }
-      expectMsg(0 millis, "Oscar Wilde wants to break dance with me!")
-      expectNoMsg(5 millis)
+      diary.expectMsg(1 millis, "Oscar Wilde wants to break dance with me!")
+      diary.expectNoMsg(5 millis)
     }
     it("should eventually lose its partner to the ravages of time") {
       machine ! "Roger B. Myerson"
-      expectMsg(50 millis) {
-        case permission.Request => testActor.reply( permission.Granted )
+      nanny.expectMsg(50 millis) {
+        // case permission.Request => nanny.testActor.reply( permission.Granted )
+        case permission.Request => machine ! permission.Granted
         case _ => fail
       }
-      expectMsg(1 millis, "Roger B. Myerson wants to break dance with me!")
-      expectMsg(15 millis, "Dancing with Roger B. Myerson was fun!")
-      expectNoMsg(50 millis)
+      diary.expectMsg(5 millis, "Roger B. Myerson wants to break dance with me!")
+      diary.expectMsg(15 millis, "Dancing with Roger B. Myerson was fun!")
+      diary.expectNoMsg(50 millis)
     }
   }
 
